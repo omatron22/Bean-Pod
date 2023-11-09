6341cc6b756a7d483629164f6128008540d6427f
Rikhil Konduru <rikhil.konduru@gmail.com>
2023-11-08 17:53:33 -0800

    This is the basic chat app that we are going to customize and develop on

---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+//add table group functionality to the ui
+//add tags
+//reskin the wholeass thing
+//make it pretty
+
+import "./App.css";
+
+import AuthPage from "./AuthPage";
+import ChatsPage from "./ChatsPage";
+
+function App() {
+  const [user, setUser] = useState(undefined);
+
+  if (!user) {
+    return <AuthPage onAuth={(user) => setUser(user)} />;
+  } else {
+    return <ChatsPage user={user} />;
+  }
+}
+
+export default App;
