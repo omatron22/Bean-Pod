01c8f702530b031fd6daac7d25a5c333be77a521
Rikhil Konduru <rikhil.konduru@gmail.com>
2023-12-04 00:15:55 -0800

    Changed directory structure to better order components

--- chat/src/App.jsx
+++ chat/src/App.jsx
@@ -1,10 +1,11 @@
 import { Home } from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Chats from "./components/Chats"
 import "./style.scss"
 function App() {
   return (
-    <Chat/>
+    <Chats/>
   );
 }
 
