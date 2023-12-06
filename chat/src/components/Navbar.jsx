18448be3cca5534f74d869446413f7799cf57117
Rikhil Konduru <rikhil.konduru@gmail.com>
2023-12-06 13:35:18 -0800

    Updated icons with material design

--- chat/src/components/Navbar.jsx
+++ chat/src/components/Navbar.jsx
@@ -21,22 +21,62 @@
 // export default Navbar
 
 
-import React from 'react';
+// import React from 'react';
+// import Logo from '../img/BeanPod_Logo.svg';
+// import { logout } from '../firebase'; // Import the logout function
+
+// const Navbar = () => {
+//   return (
+//     <div className='navbar'>
+//       <form>
+//         <img src={Logo} alt='React Image'/>
+//         <div className="name">BeanPod</div>
+//       </form>
+//       <div className="user">
+//         <img src="https://images.pexels.com/photos/16465979/pexels-photo-16465979/free-photo-of-woman-standing-in-a-rapeseed-field.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt=""/>
+//         <span>John</span>
+//         <button onClick={logout}>Logout</button> {/* Add onClick event to call logout */}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Navbar;
+
+
+import React, { useState, useEffect } from 'react';
 import Logo from '../img/BeanPod_Logo.svg';
-import { logout } from '../firebase'; // Import the logout function
+import { logout, auth } from '../firebase'; // Make sure to import auth
+import { onAuthStateChanged } from 'firebase/auth';
 
 const Navbar = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      if (currentUser) {
+        setUser(currentUser);
+      } else {
+        setUser(null);
+      }
+    });
+
+    return unsubscribe; // Clean up the subscription
+  }, []);
+
   return (
     <div className='navbar'>
       <form>
         <img src={Logo} alt='React Image'/>
         <div className="name">BeanPod</div>
       </form>
-      <div className="user">
-        <img src="https://images.pexels.com/photos/16465979/pexels-photo-16465979/free-photo-of-woman-standing-in-a-rapeseed-field.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt=""/>
-        <span>John</span>
-        <button onClick={logout}>Logout</button> {/* Add onClick event to call logout */}
-      </div>
+      {user && (
+        <div className="user">
+          <img src={user.photoURL} alt={user.displayName}/>
+          <span>{user.displayName}</span>
+          <button onClick={logout}>Logout</button>
+        </div>
+      )}
     </div>
   );
 }
--- chat/src/components/Search.jsx
+++ chat/src/components/Search.jsx
@@ -1,24 +1,3 @@
-// import React from 'react'
-
-// const Search = () => {
-//   return (
-//     <div className="search">
-//       <div className="searchForm">
-//         <input type = "text" placeholder='Find a user'/>
-//       </div>
-//       <div className = "userChat">
-//         <img src = "https://images.pexels.com/photos/16465979/pexels-photo-16465979/free-photo-of-woman-standing-in-a-rapeseed-field.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"></img>
-//         <div className = "userChatInfo">
-//           <span>Jane</span>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Search;
-
-
 import React, { useState, useEffect } from "react";
 import { collection, query, where, getDocs } from "firebase/firestore";
 import { auth, firestore as db } from "../firebase"; // Ensure this import is correct
