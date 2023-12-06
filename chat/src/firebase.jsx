d4321e161d552675bf7b001547943b7cbf03752a
Rikhil Konduru <rikhil.konduru@gmail.com>
2023-12-05 23:53:54 -0800

    Updated definition of Google Sign On to upload user data to Firestore

--- chat/src/firebase.jsx
+++ chat/src/firebase.jsx
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
+import { getFirestore, doc, setDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyBW0XTW4sPLZ7aVA3Ls71WN0ypt-2St7Lk",
@@ -15,15 +16,44 @@ initializeApp(firebaseConfig);
 
 const auth = getAuth();
 
-export const signInWithGoogle = () => {
-  signInWithPopup(auth, new GoogleAuthProvider());
+// export const signInWithGoogle = () => {
+//   signInWithPopup(auth, new GoogleAuthProvider());
+// };
+
+// export const logout = () => {
+//   signOut(auth);
+// };
+
+// export { auth };
+
+
+const firestore = getFirestore();
+const provider = new GoogleAuthProvider();
+
+export const signInWithGoogle = async () => {
+  try {
+    const result = await signInWithPopup(auth, provider);
+    const user = result.user;
+
+    // Store or update user in Firestore
+    const userRef = doc(firestore, 'users', user.uid);
+    await setDoc(userRef, {
+      displayName: user.displayName,
+      email: user.email,
+      photoURL: user.photoURL,
+    }, { merge: true });
+  } 
+  catch (error) {
+    console.error("Error signing in with Google: ", error);
+  }
+
 };
 
 export const logout = () => {
   signOut(auth);
-};
+}
 
-export { auth };
+export { auth, firestore };
 
 
 
