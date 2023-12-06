4c0a1ae9edc3cc0f5ca976a496b017ef109371a3
Robert Vernon <rcvernon@ucla.edu>
2023-12-06 02:12:20 -0800

    merged the landing and login page into one by commenting out the landing page then implementing the parallax structure to the Login.jsx file. From there I was able to implement the signInWithGoogle button into the Login page and upload our landing page picture.

--- chat/src/pages/Login.jsx
+++ chat/src/pages/Login.jsx
@@ -1,37 +1,57 @@
-// import React from 'react'
-// //import Logo from '../img/beanpodlogo.png'
-
-// const Login = () => {
-//   return (
-//     <div className='formContainer'>
-//         <div className='formWrapper'>
-//             <span className="logo">Bean Pod</span>
-//             <form>
-//                 <input type='email' placeholder='email'/>
-//                 <input type='password' placeholder='password'/>
-//                 <button>Sign in</button>
-//             </form>
-//             <p>Don't have an account yet? Register</p>
-//         </div>
-//     </div>
-//   )
-// }
-
-// export default Login;
-
-import React from 'react';
+import React, { useRef } from 'react';
 import { signInWithGoogle } from '../firebase';
+import { Parallax, ParallaxLayer } from '@react-spring/parallax';
+import beanpod from '/src/img/beanpod.png'; // Importing the beanpod image
 
 const Login = () => {
+  const ref = useRef();
   return (
-    <div className='formContainer'>
-        <div className='formWrapper'>
-            <span className="logo">Bean Pod</span>
-            <button onClick={signInWithGoogle}>Sign in with Google</button>
-            {/* Remove other form elements related to email/password login if not needed */}
-        </div>
+    <div>
+      <Parallax pages={3} ref={ref}>
+        {/* Full page background image */}
+        <ParallaxLayer
+          offset={0}
+          speed={0.5}
+          factor={3}
+          style={{
+            backgroundImage: `url(${beanpod})`,
+            backgroundSize: 'cover',
+          }}
+        />
+
+        {/* Text layer at the beginning */}
+        <ParallaxLayer
+          offset={0.2}
+          speed={0.1}
+          onClick={() => ref.current.scrollTo(2)}
+        >
+          <h2>Welcome to BeanPod</h2>
+        </ParallaxLayer>
+
+        {/* Text layer at the end */}
+        <ParallaxLayer
+          offset={2}
+          speed={0.5}
+          onClick={() => ref.current.scrollTo(0)}
+        >
+          <h2>Let's Get Started</h2>
+        </ParallaxLayer>
+
+        {/* Button layer at the end */}
+        <ParallaxLayer
+          offset={2.1}
+          speed={0.1}
+        >
+          <div className='formContainer'>
+            <div className='formWrapper'>
+              <span className="logo">Bean Pod</span>
+              <button onClick={signInWithGoogle}>Sign in with Google</button>
+            </div>
+          </div>
+        </ParallaxLayer>
+      </Parallax>
     </div>
-  )
-}
+  );
+};
 
 export default Login;
