a810b72a38b75fb62952e630d7b9176743bcc3a9
Robert Vernon <rcvernon@ucla.edu>
2023-12-06 14:12:54 -0800

    made the landing/register/login page just one webpage so no longer need to scroll witha  functional button.The image is also now on the left side of the screen and will be adjusted along with the font but this will serve as a template for later work.

--- chat/src/pages/Login.jsx
+++ chat/src/pages/Login.jsx
@@ -1,46 +1,113 @@
+// import React, { useRef } from 'react';
+// import { signInWithGoogle } from '../firebase';
+// import { Parallax, ParallaxLayer } from '@react-spring/parallax';
+// import beanpod from '/src/img/beanpod.png'; // Importing the beanpod image
+
+// const Login = () => {
+//   const ref = useRef();
+//   return (
+//     <div>
+//       <Parallax pages={3} ref={ref}>
+//         {/* Full page background image */}
+//         <ParallaxLayer
+//           offset={0}
+//           speed={0.5}
+//           factor={3}
+//           style={{
+//             backgroundImage: `url(${beanpod})`,
+//             backgroundSize: 'cover',
+//           }}
+//         />
+
+//         {/* Text layer at the beginning */}
+//         <ParallaxLayer
+//           offset={0.2}
+//           speed={0.1}
+//           onClick={() => ref.current.scrollTo(2)}
+//         >
+//           <h2>Welcome to BeanPod</h2>
+//         </ParallaxLayer>
+
+//         {/* Text layer at the end */}
+//         <ParallaxLayer
+//           offset={2}
+//           speed={0.5}
+//           onClick={() => ref.current.scrollTo(0)}
+//         >
+//           <h2>Let's Get Started</h2>
+//         </ParallaxLayer>
+
+//         {/* Button layer at the end */}
+//         <ParallaxLayer
+//           offset={2.1}
+//           speed={0.1}
+//         >
+//           <div className='formContainer'>
+//             <div className='formWrapper'>
+//               <span className="logo">Bean Pod</span>
+//               <button onClick={signInWithGoogle}>Sign in with Google</button>
+//             </div>
+//           </div>
+//         </ParallaxLayer>
+//       </Parallax>
+//     </div>
+//   );
+// };
+
+// export default Login;
+
 import React, { useRef } from 'react';
 import { signInWithGoogle } from '../firebase';
 import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 import beanpod from '/src/img/beanpod.png'; // Importing the beanpod image
 
+
 const Login = () => {
   const ref = useRef();
   return (
     <div>
-      <Parallax pages={3} ref={ref}>
+      <Parallax pages={1} ref={ref}>
         {/* Full page background image */}
         <ParallaxLayer
           offset={0}
-          speed={0.5}
-          factor={3}
+          speed={0.0}
+          factor={1}
           style={{
             backgroundImage: `url(${beanpod})`,
-            backgroundSize: 'cover',
+            backgroundSize: 'contain',
+            backgroundPosition: 'right', // Shifts the background image to the right
           }}
         />
 
-        {/* Text layer at the beginning */}
+
+
+
+        {/*Text layer at the beginning*/}
         <ParallaxLayer
-          offset={0.2}
-          speed={0.1}
-          onClick={() => ref.current.scrollTo(2)}
+          offset={0.1}
+          speed={0.0}
+          onClick={() => ref.current.scrollTo(1)}
         >
           <h2>Welcome to BeanPod</h2>
         </ParallaxLayer>
 
+
         {/* Text layer at the end */}
         <ParallaxLayer
-          offset={2}
-          speed={0.5}
-          onClick={() => ref.current.scrollTo(0)}
+          offset={0.2}
+          speed={0.0}
+          onClick={() => ref.current.scrollTo(1)}
         >
           <h2>Let's Get Started</h2>
         </ParallaxLayer>
 
+
         {/* Button layer at the end */}
         <ParallaxLayer
-          offset={2.1}
-          speed={0.1}
+          offset={0.1}
+          speed={0.0}
+
+
         >
           <div className='formContainer'>
             <div className='formWrapper'>
@@ -53,5 +120,6 @@ const Login = () => {
     </div>
   );
 };
+
 
 export default Login;
