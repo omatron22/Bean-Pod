255d351ea45d5d46bee54220ccd95db6c78e34a1
Omar Espinoza <omatron@Omars-MacBook-Air.local>
2023-12-06 20:20:25 -0800

    Added a BeanPod logo on the login page

--- chat/src/pages/Login.jsx
+++ chat/src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { signInWithGoogle } from '../firebase';
 import Logo from '/src/img/BeanPod_Logo.svg'; // Importing the beanpod logo
+import TitleImage from '/src/img/title.png'; // Importing the title image
 import googleIcon from '/src/img/google-icon.png'; // Importing Google icon
 import beanpod from '/src/img/beanpod.png'; // Importing the beanpod image
 
@@ -9,13 +10,18 @@ const Login = () => {
     <div style={{ display: 'flex', height: '100vh' }}>
       {/* Login Section */}
       <div style={{ flex: 1, display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-        {/* Wrapper for logo with increased marginTop */}
-        <div style={{ marginTop: '275px' }}> {/* Increase this value to lower the logo more */}
-          <img src={Logo} alt="Logo" style={{ width: '150px', height: 'auto' }} />
+        {/* Title Image with adjusted size and position */}
+        <div style={{ marginTop: '175px' }}> {/* Decrease this value to move the image higher */}
+          <img src={TitleImage} alt="Title" style={{ width: '325px', height: 'auto' }} /> {/* Increase width for a bigger image */}
+        </div>
+
+        {/* Beanpod Logo */}
+        <div style={{ marginTop: '40px' }}>
+          <img src={Logo} alt="Beanpod Logo" style={{ width: '150px', height: 'auto' }} />
         </div>
         
         {/* Google Sign In Button */}
-        <div className='formContainer' style={{ marginTop: '-200px' }}>
+        <div className='formContainer' style={{ marginTop: '-180px' }}>
           <button className="googleSignIn" onClick={signInWithGoogle}>
             <img src={googleIcon} alt="Google" /> Sign in with Google
           </button>
