2959d6ee6def982c339824c0e501d15528fc07d6
Omar Espinoza <omatron@Omars-MacBook-Air.local>
2023-12-06 16:45:26 -0800

    Changed styles on login page

--- chat/src/pages/Login.jsx
+++ chat/src/pages/Login.jsx
@@ -1,55 +1,28 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import { signInWithGoogle } from '../firebase';
-import { Parallax, ParallaxLayer } from '@react-spring/parallax';
+import Logo from '/src/img/BeanPod_Logo.svg'; // Importing the beanpod logo
+import googleIcon from '/src/img/google-icon.png'; // Importing Google icon
 import beanpod from '/src/img/beanpod.png'; // Importing the beanpod image
 
 const Login = () => {
-  const ref = useRef();
   return (
-    <div>
-      <Parallax pages={3} ref={ref}>
-        {/* Full page background image */}
-        <ParallaxLayer
-          offset={0}
-          speed={0.5}
-          factor={3}
-          style={{
-            backgroundImage: `url(${beanpod})`,
-            backgroundSize: 'cover',
-          }}
-        />
-
-        {/* Text layer at the beginning */}
-        <ParallaxLayer
-          offset={0.2}
-          speed={0.1}
-          onClick={() => ref.current.scrollTo(2)}
-        >
-          <h2>Welcome to BeanPod</h2>
-        </ParallaxLayer>
-
-        {/* Text layer at the end */}
-        <ParallaxLayer
-          offset={2}
-          speed={0.5}
-          onClick={() => ref.current.scrollTo(0)}
-        >
-          <h2>Let's Get Started</h2>
-        </ParallaxLayer>
-
-        {/* Button layer at the end */}
-        <ParallaxLayer
-          offset={2.1}
-          speed={0.1}
-        >
-          <div className='formContainer'>
-            <div className='formWrapper'>
-              <span className="logo">Bean Pod</span>
-              <button onClick={signInWithGoogle}>Sign in with Google</button>
-            </div>
+    <div style={{ display: 'flex', height: '100vh' }}>
+      {/* Login Section */}
+      <div style={{ flex: 1, display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+        <img src={Logo} alt="Logo" style={{ marginBottom: '20px' }} />
+        <div className='formContainer'>
+          <div className='formWrapper'>
+            <button className="googleSignIn" onClick={signInWithGoogle}>
+              <img src={googleIcon} alt="Google" /> Sign in with Google
+            </button>
           </div>
-        </ParallaxLayer>
-      </Parallax>
+        </div>
+      </div>
+
+      {/* Image Section */}
+      <div style={{ flex: 1, backgroundImage: `url(${beanpod})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+        {/* This div will show the image */}
+      </div>
     </div>
   );
 };
