7c2c994c14f54cb3fa19f16c0798f747e9ef5787
Rikhil Konduru <rikhil.konduru@gmail.com>
2023-12-08 12:24:34 -0800

    Added Ant Design support

--- chat/src/App.jsx
+++ chat/src/App.jsx
@@ -1,76 +1,9 @@
-// // import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-// // import { Home } from "./pages/Home";
-// // import Login from "./pages/Login";
-// // import { useEffect, useState } from 'react';
-// // import { auth } from './firebase';
-// // import "./style.scss"
-
-// // function App() {
-// //   const [currentUser, setCurrentUser] = useState(null);
-
-// //   useEffect(() => {
-// //     const unsubscribe = auth.onAuthStateChanged(user => {
-// //       setCurrentUser(user);
-// //     });
-// //     return unsubscribe; // Unsubscribe on unmount
-// //   }, []);
-
-// //   return (
-// //     <Router>
-// //       <Routes>
-// //         <Route path="/" element={currentUser ? <Home /> : <Navigate to="/login" replace />} />
-// //         <Route path="/login" element={!currentUser ? <Login /> : <Navigate to="/" replace />} />
-// //         <Route path="*" element={<Navigate to={currentUser ? "/" : "/login"} replace />} />
-// //       </Routes>
-// //     </Router>
-// //   );
-// // }
-
-// // export default App;
-
-// import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-// import { Home } from "./pages/Home";
-// import Login from "./pages/Login";
-// import { useEffect, useState } from 'react';
-// import { auth } from './firebase';
-// import "./style.scss";
-// import { ChatContextProvider } from './context/ChatContext'; // Import ChatContextProvider
-// import { AuthContextProvider } from './context/AuthContext';
-
-// function App() {
-//   // const [currentUser, setCurrentUser] = useState(null);
-
-//   // useEffect(() => {
-//   //   const unsubscribe = auth.onAuthStateChanged(user => {
-//   //     setCurrentUser(user);
-//   //   });
-//   //   return unsubscribe; // Unsubscribe on unmount
-//   // }, []);
-
-//   const { currentUser } = useContext(AuthContext);
-
-//   return (
-//     <Router>
-//       <AuthContextProvider>
-//         <ChatContextProvider> {/* Wrap Routes with ChatContextProvider */}
-//           <Routes>
-//             <Route path="/" element={currentUser ? <Home /> : <Navigate to="/login" replace />} />
-//             <Route path="/login" element={!currentUser ? <Login /> : <Navigate to="/" replace />} />
-//             <Route path="*" element={<Navigate to={currentUser ? "/" : "/login"} replace />} />
-//           </Routes>
-//         </ChatContextProvider>
-//       </AuthContextProvider>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
-import React, { useContext } from 'react'; // Import useContext here
+import React, { useContext } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import { Home } from "./pages/Home";
 import Login from "./pages/Login";
-import "./style.scss";
+//import 'antd/dist/antd.css'; // Import Ant Design CSS
+import { Layout } from 'antd'; // Import Layout component from Ant Design
 import { ChatContextProvider } from './context/ChatContext';
 import { AuthContextProvider, AuthContext } from './context/AuthContext';
 
@@ -79,11 +12,13 @@ function App() {
     <Router>
       <AuthContextProvider>
         <ChatContextProvider>
-          <Routes>
-            <Route path="/" element={<PrivateRoute />}/>
-            <Route path="/login" element={<PublicRoute />}/>
-            <Route path="*" element={<Navigate to="/" replace />} />
-          </Routes>
+          <Layout style={{ minHeight: '100vh' }}> {/* Use Ant Design Layout */}
+            <Routes>
+              <Route path="/" element={<PrivateRoute />}/>
+              <Route path="/login" element={<PublicRoute />}/>
+              <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
+          </Layout>
         </ChatContextProvider>
       </AuthContextProvider>
     </Router>
