28676638df10025d96f06e6b52f82d218a13ae7c
Rikhil Konduru <rikhil.konduru@gmail.com>
2023-12-08 15:50:21 -0800

    Solved a couple rendering bugs and adjusted chat padding

--- chat/src/components/Chat.jsx
+++ chat/src/components/Chat.jsx
@@ -1,59 +1,54 @@
-import React, { useContext } from "react";
-import Add from "../img/add.svg";
-import Messages from "./Messages";
-import Input from "./Input";
-import { ChatContext } from "../context/ChatContext";
-
-const Chat = () => {
-  const { data } = useContext(ChatContext);
-
-  return (
-    <div className="chat">
-      <div className="chatInfo">
-        <span>{data.user?.displayName}</span>
-        <div className="chatIcons">
-          <img src={Add} alt="" />
-        </div>
-      </div>
-      <Messages />
-      <Input/>
-    </div>
-  );
-};
-
-export default Chat;
-
 // import React, { useContext } from "react";
-// import { Card, Typography, Avatar, Space } from 'antd';
-// import { UserOutlined } from '@ant-design/icons';
+// import Add from "../img/add.svg";
 // import Messages from "./Messages";
 // import Input from "./Input";
 // import { ChatContext } from "../context/ChatContext";
-// import Add from "../img/add.svg"; // Make sure this icon is compatible or use an Ant Design icon
-
-// const { Title } = Typography;
 
 // const Chat = () => {
 //   const { data } = useContext(ChatContext);
 
 //   return (
-//     <Card className="chat">
-//       <Card.Header
-//         title={<Title level={4}>{data.user?.displayName || "Select a chat"}</Title>}
-//         avatar={
-//           data.user ? <Avatar src={data.user.photoURL} /> : <Avatar icon={<UserOutlined />} />
-//         }
-//         extra={
-//           <Space>
-//             <Avatar src={Add} alt="" shape="circle" />
-//             {/* Add more icons or buttons here if needed */}
-//           </Space>
-//         }
-//       />
+//     <div className="chat">
+//       <div className="chatInfo">
+//         <span>{data.user?.displayName}</span>
+//         <div className="chatIcons">
+//           <img src={Add} alt="" />
+//         </div>
+//       </div>
 //       <Messages />
-//       <Input />
-//     </Card>
+//       <Input/>
+//     </div>
 //   );
 // };
 
 // export default Chat;
+
+import React, { useContext } from "react";
+import { Card, Typography, Avatar, Space } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
+import Messages from "./Messages";
+import Input from "./Input";
+import { ChatContext } from "../context/ChatContext";
+import Add from "../img/add.svg"; // Ensure this icon is compatible
+
+const { Title } = Typography;
+
+const Chat = () => {
+  const { data } = useContext(ChatContext);
+
+  return (
+    <Card className="chat">
+      <Space direction="vertical" style={{ width: '100%' }}>
+        <Space>
+          {<Avatar src={data.user.photoURL} />}
+          <Title level={4}>{data.user?.displayName || "Select a chat"}</Title>
+          {/* Add more icons or buttons here if needed */}
+        </Space>
+        <Messages />
+        <Input />
+      </Space>
+    </Card>
+  );
+};
+
+export default Chat;
--- chat/src/components/Navbar.jsx
+++ chat/src/components/Navbar.jsx
@@ -1,51 +1,71 @@
-import React, { useContext } from 'react';
-import Logo from '../img/BeanPod_Logo.svg';
-import { logout } from '../firebase';
-import { AuthContext } from '../context/AuthContext'
-import { Layout, Avatar, Menu, Button } from 'antd';
-import { UserOutlined } from '@ant-design/icons';
+// import React, { useContext } from 'react';
+// import Logo from '../img/BeanPod_Logo.svg'; // Your imported logo
+// import { logout } from '../firebase';
+// import { AuthContext } from '../context/AuthContext';
+// import { Layout, Avatar, Menu, Button } from 'antd';
+// import { UserOutlined } from '@ant-design/icons';
 
-const Navbar = () => {
-
-  const {currentUser} = useContext(AuthContext)
-  const { Header } = Layout;
+// const Navbar = () => {
+//   const { currentUser } = useContext(AuthContext);
+//   const { Header } = Layout;
 
 //   return (
-//     <div className='navbar'>
+//     <Header style={{ 
+//       display: 'flex: 1', 
+//       alignItems: 'center', 
+//       backgroundColor: '#ffffff',
+//       justifyContent: 'space-between', // Ensures the logo and menu are on opposite sides
+//       padding: '0 30px' // Adjust the horizontal padding as needed
+//     }}>
 //       {currentUser && (
-//         <div className="user">
-//           <form>
-//             <img src={currentUser.photoURL} alt={currentUser.displayName}/>
-//             <span>{currentUser.displayName}</span>
-//           </form> 
-//           <form>
-//             <button onClick={logout}>Logout</button>
-//           </form>
-//         </div>
+//         <Menu mode="horizontal" style={{ flex: 1, justifyContent: 'end' }}>
+//           <Menu.Item key="user" icon={<Avatar src={currentUser.photoURL || <UserOutlined />} />}>
+//             {currentUser.displayName}
+//           </Menu.Item>
+//           <Menu.Item key="logout">
+//             <Button type="primary" onClick={logout}>Logout</Button>
+//           </Menu.Item>
+//         </Menu>
 //       )}
-//     </div>
+//     </Header>
 //   );
-// }
+// };
 
 // export default Navbar;
 
-return (
-  <Header style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', backgroundColor: '#f0f2f5' }}>
-    <div>
-      {/* Logo or title */}
-    </div>
-    {currentUser && (
-      <Menu mode="horizontal">
-        <Menu.Item key="user" icon={<Avatar src={currentUser.photoURL || <UserOutlined />} />}>
-          {currentUser.displayName}
-        </Menu.Item>
-        <Menu.Item key="logout">
-          <Button type="primary" onClick={logout}>Logout</Button>
-        </Menu.Item>
-      </Menu>
-    )}
-  </Header>
-);
+import React, { useContext } from 'react';
+import { Layout, Avatar, Menu, Button } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
+import { AuthContext } from '../context/AuthContext';
+import { logout } from '../firebase';
+
+const Navbar = () => {
+  const { currentUser } = useContext(AuthContext);
+  const { Header } = Layout;
+
+  return (
+    <Header style={{ 
+      display: 'flex', 
+      alignItems: 'center', 
+      backgroundColor: '#ffffff',
+      justifyContent: 'space-between',
+      padding: '0px'
+    }}>
+      {currentUser && (
+        <Menu mode="horizontal" style={{ flex: 1, justifyContent: 'end', lineHeight: '64px' }}>
+          <Menu.Item key="user" style={{ display: 'flex', alignItems: 'center' }}>
+            <Avatar src={currentUser.photoURL || <UserOutlined />} style={{ marginRight: 10 }} />
+            {currentUser.displayName}
+          </Menu.Item>
+          <Menu.Item key="logout">
+            <Button type="primary" onClick={logout}>Logout</Button>
+          </Menu.Item>
+        </Menu>
+      )}
+    </Header>
+  );
 };
 
 export default Navbar;
+
+
